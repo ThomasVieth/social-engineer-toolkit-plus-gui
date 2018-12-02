2947aa799e5cebddffa3f029b57542bdc462412f
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2018-12-02 20:22:29 +0000

    Initial window implementation extended from Tkinter.
    Using Tkinter constructed a Window class that stores a list of any windows created and generates each expecting an owner to be provided.

---
+++ core/gui/window.py
@@ -0,0 +1,26 @@
+##
+##
+##
+##
+##
+
+## Imports
+from tkinter import Tk
+
+## All declaration for import *
+__all__ = ('Window', )
+
+## Window class extended from tkinter
+
+class Window(Tk):
+	all_windows = list()
+
+	def __init__(self, owner, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+		self._owner = owner
+
+	def __new__(cls, *args, **kwargs):
+		instance = cls(*args, **kwargs)
+		all_windows.append(instance)
+		return instance
