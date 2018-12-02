cd3f45c8a13895bc0590347aa3337c1b8ddd4c43
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2018-12-02 20:24:16 +0000

    Added a classproperty dectorator.
    Created a class property decorator to be used inside hangable_windows.py (a later commited item).

---
+++ core/helpers.py
@@ -0,0 +1,24 @@
+##
+##
+##
+##
+##
+
+## Imports
+
+## Class property decorator
+
+class classproperty(object):
+	'A new descriptor for stand-alone properties inside classes.'
+	
+    def __init__(self, fget=None, doc=None):
+    	'Initiates the getter and doc-string for the property.'
+        self.fget = fget
+        if doc is None and fget is not None:
+            doc = fget.__doc__
+        self.__doc__ = doc
+
+    def __get__(self, obj, cls=None):
+    	if cls is None:
+			return self.fget(type(obj))
+		return self.fget(cls)
