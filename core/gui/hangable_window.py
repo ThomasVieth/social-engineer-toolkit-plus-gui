0837293789166e7b4c9a7d8078046617803ebc19
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2018-12-02 20:25:41 +0000

    Extended Window to HangableWindow.
    Created an extension of the Window class to enable features and widgets inside of a window to be hangable whenever required. Also added tracking to hanging windows specifically.

---
+++ core/gui/hangable_window.py
@@ -0,0 +1,41 @@
+##
+##
+##
+##
+##
+
+## Imports
+from .window import Window
+
+from ..helpers import classproperty
+
+## All declaration for import *
+__all__ = ('HangableWindow', )
+
+## HangableWindow class extended from window.<Window>
+
+class HangableWindow(Window):
+
+	def __init__(self, *args **kwargs):
+		super().__init__(*args, **kwargs)
+
+		self.is_hanging = False
+
+	@classmethod
+	def hangable_method(cls, method):
+		def decorator(self, *args, **kwargs):
+			if not self.is_hanging:
+				return method(self, *args, **kwargs)
+		return decorator
+
+	@classproperty
+	def hangable_windows(cls):
+		return [window for window in cls.all_windows if hasattr(window, 'is_hanging')]
+
+	@classproperty
+	def hanging_windows(cls):
+		return [window for window in cls.all_windows if hasattr(window, 'is_hanging') and window.is_hanging is True]
+
+	@classproperty
+	def non_hanging_windows(cls):
+		return [window for window in cls.all_windows if hasattr(window, 'is_hanging') and window.is_hanging is False]
