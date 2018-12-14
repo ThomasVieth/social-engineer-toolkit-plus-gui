834f23c27ad5267504d591d29e6f2b5a867bbe4a
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2018-12-14 12:31:12 +0000

    Fixed consistency in indentation.
    The repo was inconsistently using tabs and spaces so corrected to use 4 space indents.

--- core/gui/hangable_window.py
+++ core/gui/hangable_window.py
@@ -16,26 +16,26 @@ __all__ = ('HangableWindow', )
 
 class HangableWindow(Window):
 
-	def __init__(self, *args **kwargs):
-		super().__init__(*args, **kwargs)
+    def __init__(self, *args **kwargs):
+        super().__init__(*args, **kwargs)
 
-		self.is_hanging = False
+        self.is_hanging = False
 
-	@classmethod
-	def hangable_method(cls, method):
-		def decorator(self, *args, **kwargs):
-			if not self.is_hanging:
-				return method(self, *args, **kwargs)
-		return decorator
+    @classmethod
+    def hangable_method(cls, method):
+        def decorator(self, *args, **kwargs):
+            if not self.is_hanging:
+                return method(self, *args, **kwargs)
+        return decorator
 
-	@classproperty
-	def hangable_windows(cls):
-		return [window for window in cls.all_windows if hasattr(window, 'is_hanging')]
+    @classproperty
+    def hangable_windows(cls):
+        return [window for window in cls.all_windows if hasattr(window, 'is_hanging')]
 
-	@classproperty
-	def hanging_windows(cls):
-		return [window for window in cls.all_windows if hasattr(window, 'is_hanging') and window.is_hanging is True]
+    @classproperty
+    def hanging_windows(cls):
+        return [window for window in cls.all_windows if hasattr(window, 'is_hanging') and window.is_hanging is True]
 
-	@classproperty
-	def non_hanging_windows(cls):
-		return [window for window in cls.all_windows if hasattr(window, 'is_hanging') and window.is_hanging is False]
+    @classproperty
+    def non_hanging_windows(cls):
+        return [window for window in cls.all_windows if hasattr(window, 'is_hanging') and window.is_hanging is False]
--- core/gui/window.py
+++ core/gui/window.py
@@ -13,14 +13,14 @@ __all__ = ('Window', )
 ## Window class extended from tkinter
 
 class Window(Tk):
-	all_windows = list()
+    all_windows = list()
 
-	def __init__(self, owner, *args, **kwargs):
-		super().__init__(*args, **kwargs)
+    def __init__(self, owner, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-		self._owner = owner
+        self._owner = owner
 
-	def __new__(cls, *args, **kwargs):
-		instance = cls(*args, **kwargs)
-		all_windows.append(instance)
-		return instance
+    def __new__(cls, *args, **kwargs):
+        instance = cls(*args, **kwargs)
+        all_windows.append(instance)
+        return instance
