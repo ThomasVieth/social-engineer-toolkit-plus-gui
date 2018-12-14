2ae4a2d70181e55758a874d8d1d195be3a937399
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2018-12-14 13:39:11 +0000

    Changed indentation standard again...

--- config/__init__.py
+++ config/__init__.py
@@ -20,15 +20,15 @@ cache = dict()
 ## Parsing of the configuration file
 
 def get_config_parser_for(name):
-	'Returns a configparser.ConfigParser object with the configuration file specified parsed in.'
-	if name in cache:
-		return cache[name]
+    'Returns a configparser.ConfigParser object with the configuration file specified parsed in.'
+    if name in cache:
+        return cache[name]
 
-	file = join(current_working_directory, name + '.ini')
-	if not isfile(file):
-		raise ValueError('Configuration file for ' + name + ' is non-existent.')
+    file = join(current_working_directory, name + '.ini')
+    if not isfile(file):
+        raise ValueError('Configuration file for ' + name + ' is non-existent.')
 
-	parser = ConfigParser()
-	parser.read(file)
-	cache[name] = parser
-	return parser
+    parser = ConfigParser()
+    parser.read(file)
+    cache[name] = parser
+    return parser
