b07d98d071ecbf2590937fe45f0a7ce47ff24d91
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2019-01-25 16:40:27 +0000

    Test menus added for proof of concept.

--- __init__.py
+++ __init__.py
@@ -6,10 +6,4 @@
 
 ## Imports
 
-from .core.display import create_and_display_section
-from .modules import main_menu
-
-##
-
-def run():
-    main_window = create_and_display_section(main_menu)
+from .modules import *
--- modules/__init__.py
+++ modules/__init__.py
@@ -6,14 +6,55 @@
 
 ## Imports
 
-from ..core.options import Section, Option
+from ..core.windows import OptionWindow
 
 ##
 
-main_menu = Section("SE Toolkit GUI")
-main_menu.append(Option("Social-Engineering Attacks", lambda x: print("Hello1")))
-main_menu.append(Option("Penetration Testing (Fast-Track)", lambda x: print("Hello2")))
-main_menu.append(Option("Third Party Modules", lambda x: print("Hello3")))
-main_menu.append(Option("Update the Social-Engineer Toolkit", lambda x: print("Hello4")))
-main_menu.append(Option("Update SET configuration", lambda x: print("Hello5")))
-main_menu.append(Option("Help, Credits, and About", lambda x: print("Hello6")))
+class MainWindow(OptionWindow):
+
+    @OptionWindow.method_as_option("1. Hang This Window")
+    @OptionWindow.hangable_method
+    def test1(self, *args, **kwargs):
+        self.is_hanging = True
+
+    @OptionWindow.method_as_option("2. Unhang Other Window")
+    @OptionWindow.hangable_method
+    def test2(self, *args, **kwargs):
+        main_menu2.is_hanging = False
+
+    @OptionWindow.method_as_option("3. Is Other Window Hanging?")
+    @OptionWindow.hangable_method
+    def test3(self, *args, **kwargs):
+        print(main_menu2.is_hanging)
+
+    def destroy(self, *args, **kwargs):
+        super().destroy(*args, **kwargs)
+
+        main_menu2.is_hanging = False
+
+class MainWindow2(OptionWindow):
+
+    @OptionWindow.method_as_option("1. Hang This Window")
+    @OptionWindow.hangable_method
+    def test1(self, *args, **kwargs):
+        self.is_hanging = True
+
+    @OptionWindow.method_as_option("2. Unhang Other Window")
+    @OptionWindow.hangable_method
+    def test2(self, *args, **kwargs):
+        main_menu.is_hanging = False
+
+    @OptionWindow.method_as_option("3. Is Other Window Hanging?")
+    @OptionWindow.hangable_method
+    def test3(self, *args, **kwargs):
+        print(main_menu.is_hanging)
+
+    def destroy(self, *args, **kwargs):
+        super().destroy(*args, **kwargs)
+
+        main_menu.is_hanging = False
+
+##
+
+main_menu = MainWindow("SE Toolkit GUI 1", None)
+main_menu2 = MainWindow2("SE Toolkit GUI 2", main_menu)
